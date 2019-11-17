4fc35126998209d5e6141ecc5dd8b10c5115274e
Krzysztof Bukowski <kbukowski@sii.pl>
2019-11-17 20:44:16 +0100

    task-17 Configured github actions

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,66 @@
-const path = require("path");
+/* eslint-disable */
+const path = require('path');
 
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV,
 
   entry: {
-    main: "./front-end/src/index.tsx"
+    main: './front-end/src/index.tsx',
   },
 
   output: {
-    filename: "[name].[contenthash:5].js",
-    path: path.resolve(__dirname, "build")
+    filename: '[name].[contenthash:5].js',
+    path: path.resolve(__dirname, 'build'),
   },
 
   devServer: {
     open: true,
-    port: process.env.WEBPACK_PORT || 9000
+    port: process.env.WEBPACK_PORT || 9000,
   },
 
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader"
+        use: 'ts-loader',
       },
 
       {
         test: /\.(jpg|png|svg|gif)$/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "[name].[contenthash:5].[ext]"
-        }
+          name: '[name].[contenthash:5].[ext]',
+        },
       },
 
       {
         test: /\.(scss|sass)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader", 
-          "sass-loader"
-        ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
 
       {
         test: /\.(jsx?)$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
-          presets: [
-            "@babel/preset-env",
-            "@babel/preset-react"
-          ]
-        }
-      }
-
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
     ],
   },
 
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin(
-      {
-        title: "test",
-        template: "front-end/index.html"
-      }
-    ),
-    new MiniCssExtractPlugin(
-      {
-        filename: "[name].[contenthash:5].css"
-      }
-    )
-  ]
-}
- 
+    new HtmlWebPackPlugin({
+      title: 'test',
+      template: 'front-end/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:5].css',
+    }),
+  ],
+};
