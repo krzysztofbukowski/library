d0b269f2f9254f341694a870ed4859d915ebf5cb
Krzysztof Bukowski <kbukowski@sii.pl>
2019-11-22 13:46:52 +0100

    task-12 Fixed webpack formatting

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,11 @@ module.exports = {
         loader: "babel-loader",
         exclude: /node_modules/,
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"]
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react",
+            "@babel/preset-typescript"
+          ]
         }
       }
     ]
