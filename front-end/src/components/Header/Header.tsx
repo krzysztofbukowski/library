63ac433e220d047da8c9a41c18ba026d6bb3ce3b
Elfant <alexs98@wp.pl>
2019-12-04 13:57:41 +0100

    task-13 Update

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -2,26 +2,27 @@ import * as React from 'react';
 
 import * as styles from './Header.scss';
 
-import Menu from "../Menu";
+import Menu from '../Menu';
 import Search from '../Search';
 
-import logo from "../../assets/logo.svg";
+import logo from '../../assets/logo.svg';
 
 const Header: React.FC = () => (
   <div className={styles.header}>
     <div className={`container ${styles.headerBox}`}>
-    <div className={styles.logoBox}>
-      <img src={logo} alt="" className={styles.logoImg}/>
-      <div className={styles.logoDesc}>
-        Biblioteka Szkoły Podstawowej w Pieniężnie
+      <div className={styles.logoBox}>
+        <img src={logo} alt="" className={styles.logoImg} />
+        <div className={styles.logoDesc}>
+          Biblioteka Szkoły Podstawowej w Pieniężnie
+        </div>
       </div>
-    </div>
-    <div className={styles.SearchBox}>
-      <Search />
-    </div>
-    <label>Menu
-      <Menu />
-    </label>
+      <div className={styles.SearchBox}>
+        <Search />
+      </div>
+      <label>
+        Menu
+        <Menu />
+      </label>
     </div>
   </div>
 );
--- front-end/src/components/Menu/Menu.tsx
+++ front-end/src/components/Menu/Menu.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
+import * as React from 'react';
 
-import style from "./Menu.scss"; 
+import style from './Menu.scss';
 
-const Menu = () => (
-    <i className={`${style.icon} fas fa-hamburger`}></i>
+const Menu: React.FC = () => (
+  <i className={`${style.icon} fas fa-hamburger`}></i>
 );
 
 export default Menu;
