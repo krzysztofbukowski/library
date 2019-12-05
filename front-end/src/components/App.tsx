d225312e33506a6cefb0b3e259c6e922418e3513
Elfant <alexs98@wp.pl>
2019-12-05 21:00:30 +0100

    task-30 Define 2-column layout

--- front-end/src/components/App.tsx
+++ front-end/src/components/App.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 
 import Header from './Header';
-import Footer from './Footer';
+import Carousel from './Carousel';
 import Content from './Content';
+import Footer from './Footer';
 
 class App extends React.Component {
   render(): JSX.Element {
     return (
       <>
         <Header />
+        <Carousel />
         <div className="container">
           <Content />
         </div>
--- front-end/src/components/Content/Content.tsx
+++ front-end/src/components/Content/Content.tsx
@@ -4,7 +4,10 @@ import * as styles from './Content.scss';
 
 const Content: React.FC = () => (
   <main>
-    <div className={styles.content}></div>
+    <div className={styles.content}>
+      <div className={styles.popularBooks}></div>
+      <div className={styles.lists}></div> {/*Temporary name of class*/}
+    </div>
   </main>
 );
 
