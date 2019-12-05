fb1affa49d1a056687fe10901507598ddf9cb9f9
Elfant <alexs98@wp.pl>
2019-12-05 21:38:56 +0100

    task-30 Update

--- front-end/src/components/Content/Content.tsx
+++ front-end/src/components/Content/Content.tsx
@@ -5,8 +5,8 @@ import * as styles from './Content.scss';
 const Content: React.FC = () => (
   <main>
     <div className={styles.content}>
-      <div className={styles.popularBooks}></div>
-      <div className={styles.lists}></div> {/*Temporary name of class*/}
+      <div className={styles.main}></div>
+      <div className={styles.aside}></div>
     </div>
   </main>
 );
