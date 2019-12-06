78d9fa5505e43e1797f2574a7f052284fc185bfb
Elfant <alexs98@wp.pl>
2019-12-07 00:24:50 +0100

    LIB-19 Define 2-column layout

---
+++ front-end/src/components/Carousel/Carousel.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+import * as styles from './Carousel.scss';
+
+const Carousel: React.FC = () => <div className={styles.carousel}></div>;
+
+export default Carousel;
--- front-end/src/components/Content/Content.tsx
+++ front-end/src/components/Content/Content.tsx
@@ -3,9 +3,12 @@ import * as React from 'react';
 import * as styles from './Content.scss';
 
 const Content: React.FC = () => (
-  <main>
-    <div className={styles.content}></div>
-  </main>
+  <>
+    <main className={styles.main}>
+      <div className={styles.content}></div>
+    </main>
+    <aside className={styles.aside} />
+  </>
 );
 
 export default Content;
