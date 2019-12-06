33f3dc624368e1457a2d5a2872b1f408e1b41533
Elfant <alexs98@wp.pl>
2019-12-06 23:32:20 +0100

    LIB-19 Update

--- front-end/src/components/Content/Content.tsx
+++ front-end/src/components/Content/Content.tsx
@@ -5,10 +5,9 @@ import * as styles from './Content.scss';
 const Content: React.FC = () => (
   <>
     <main className={styles.main}>
-      <div className={styles.content}>
-      </div>
+      <div className={styles.content}></div>
     </main>
-    <aside className={styles.aside}/>
+    <aside className={styles.aside} />
   </>
 );
 
